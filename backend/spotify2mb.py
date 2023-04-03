50c5c09a4a9029a5f978b3e711b19a0ed42654ba
Eise de Vries <16731936+eisedevries@users.noreply.github.com>
2023-04-03 17:33:25 +0200

    spotify2musicbrainz 2.0

---
+++ backend/spotify2mb.py
@@ -0,0 +1,38 @@
+
+import musicbrainzngs
+
+musicbrainzngs.set_useragent("BigMusic", 1.0)
+
+
+
+
+# TODO change default of album
+def make_spotify_url(spotify_id, spotify_type="album"):
+    spotify_type = 'artist'
+    return "https://open.spotify.com/{0}/{1}".format(spotify_type, spotify_id)
+
+
+
+
+def get_artist_mbid(spotify_id):
+    spotify_url = make_spotify_url(spotify_id, "artist")
+    try:
+        mb_url = musicbrainzngs.browse_urls(spotify_url,
+                                            includes=["artist-rels"])
+    except musicbrainzngs.musicbrainz.ResponseError:
+        return "URL doesn't exist."
+
+    if "artist-relation-list" in mb_url["url"]:
+        artists = mb_url["url"]["artist-relation-list"]
+        if len(artists) == 1:
+            return artists[0]["artist"]["id"]
+        elif len(artists) > 1:
+            return "More than one associated artist."
+    else:
+        return "No associated artists."
+
+
+
+
+# Nirvana id to MBID
+print(get_artist_mbid('6olE6TJLqED3rqDCT0FyPh'))
--- backend/test_call.py
+++ backend/test_call.py
@@ -4,7 +4,7 @@ from data_merger import get_artist_results
 #print(get_artist_results('6olE6TJLqED3rqDCT0FyPh'))
 
 # Neutral Milk Hotel call
-print(get_artist_results('2ooIqOf4X2uz4mMptXCtie'))
+#print(get_artist_results('2ooIqOf4X2uz4mMptXCtie'))
 
-
-
+# John Williams (gitarist)
+print(get_artist_results('6mBYeMZZUhJKEvRXagJYzY'))
